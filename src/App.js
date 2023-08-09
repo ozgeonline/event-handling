88bd3e2bb30252589596188891532ea757bf50af
ozgeonline <nisroc.414@hotmail.com>
2023-08-09 09:50:02 +0300

    handling test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+  const [isMouseOver,setMouseOver] = useState(false);
+  function handleMouseOver(){
+    setMouseOver(true)
+  }
+  function handleMouseOut(){
+    setMouseOver(false)
+  }
+
+  const [headingText,setHeadingText] = useState("Hello");
+  function handleClick(event){
+    setHeadingText("Submitted")
+    setName("")
+    event.preventDefault();
+  }
+
+  const [name,setName] = useState("");
+  function handleChange(event){
+    console.log(event.target.value);
+    // console.log(event.target.placeholder);
+    // console.log(event.target.type);
+    setName(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>{headingText} {name}</h1>
+      <form action="" onSubmit={handleClick}>
+        <input type="text" placeholder="What's your name?" value={name} onChange={handleChange} />
+        <button
+        type='submit'
+          onMouseOver={handleMouseOver}
+          onMouseLeave={handleMouseOut}
+          style={{backgroundColor: isMouseOver ? "black" : "white"}}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
